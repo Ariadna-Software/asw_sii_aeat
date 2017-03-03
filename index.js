cc132e0edd3cb04eae034e3cc9a643c1c4e949f6
Rafael Garcia <rafa@myariadna.com>
2017-03-03 17:53:30 +0100

    Caso consulta real

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 console.log("(C) Ariadna Software SL // SII");
+console.log("-- NODE SII START --");
+var soap = require('soap');
+var fs = require('fs');
+
+
+var url = 'http://www.agenciatributaria.es/static_files/AEAT/Contenidos_Comunes/La_Agencia_Tributaria/Modelos_y_formularios/Suministro_inmediato_informacion/FicherosSuministros/V_05/SuministroFactEmitidas.wsdl';
+soap.createClient(url, function (err, client) {
+    if (err) {
+        console.log(err);
+    } else {
+        var obb = client.describe();
+        var input = obb.siiService.SuministroFactEmitidasPruebas.ConsultaLRFacturasEmitidas.input;
+        var myInput = input;
+        myInput = {
+            Cabecera: {
+                IDVersionSii: "0.5",
+                Titular: {
+                    NombreRazon: "Ariadna Software SL",
+                    NIF: "B96470190"
+                }
+            },
+            FiltroConsulta: {
+                PeriodoImpositivo: {
+                    Ejercicio: "2017",
+                    Periodo: "02"
+                }
+            }
+        };
+        client.setSecurity( new soap.ClientSSLSecurityPFX("/tmp/ariadna_sii.pfx","passsii"));
+        client.siiService.SuministroFactEmitidasPruebas.ConsultaLRFacturasEmitidas(myInput, function (err, result, raw, soapHeader) {
+            if (err) {
+                // console.log(err);
+                console.log(raw);
+            }
+        })
+    }
+});
