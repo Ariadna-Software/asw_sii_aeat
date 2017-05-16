57c02dbbc0751ed62d955314023066949ba680e5
Rafael Garcia <rafa@myariadna.com>
2017-05-16 08:36:27 +0200

    Emisores idioma

---
+++ public/js/app/emisoresDetalle.js
@@ -0,0 +1,81 @@
+﻿/*
+ titulares.js
+ Funciones propias de la página Titulares.html
+*/
+
+var titular = apiComunGeneral.obtenerUsuario();
+var data = null;
+var gruposTitularId = 0;
+var vm;
+
+var apiTitularesDetalle = {
+    ini: function () {
+        apiComunGeneral.initPage(titular);
+        apiComunAjax.establecerClave(titular.apiKey);
+
+        vm = new apiTitularesDetalle.datosPagina();
+        ko.applyBindings(vm);
+
+        $('#titulares').attr('class', 'active');
+        $('#titular-form').submit(function () { return false; });
+        $('#btnAceptar').click(apiTitularesDetalle.aceptar);
+        $('#btnSalir').click(apiTitularesDetalle.salir);
+
+        titularId = apiComunGeneral.gup("id");
+        if (titularId == 0) {
+            vm.titularId(0);
+        } else {
+            apiTitularesDetalle.cargarTitular(titularId);
+        }
+    },
+    cargarTitular: function (id) {
+        apiComunAjax.llamadaGeneral("GET", myconfig.apiUrl + "/api/titulares/" + id, null, function (err, data) {
+            if (err) return;
+            apiTitularesDetalle.cargarDatosPagina(data);
+        });
+    },
+    cargarDatosPagina: function (data) {
+        vm.titularId(data.titularId);
+        vm.nombreRazon(data.nombreRazon);
+        vm.nifTitular(data.nifTitular);
+        vm.nifRepresentante(data.nifRepresentante);
+    },
+    datosPagina: function () {
+        var self = this;
+        self.titularId = ko.observable();
+        self.nombreRazon = ko.observable();
+        self.nifTitular = ko.observable();
+        self.nifRepresentante = ko.observable();
+    },
+    aceptar: function () {
+        if (!apiTitularesDetalle.datosOk()) return;
+        var data = {
+            titularId: vm.titularId(),
+            nombreRazon: vm.nombreRazon(),
+            nifTitular: vm.nifTitular(),
+            nifRepresentante: vm.nifRepresentante()
+        };
+        var verb = "PUT";
+        if (vm.titularId() == 0) verb = "POST";
+        apiComunAjax.llamadaGeneral(verb, myconfig.apiUrl + "/api/titulares", data, function (err, data) {
+            if (err) return;
+            apiTitularesDetalle.salir();
+        });
+    },
+    datosOk: function () {
+        $('#titular-form').validate({
+            rules: {
+                txtNombreRazon: { required: true }
+            },
+            errorPlacement: function (error, element) {
+                error.insertAfter(element.parent());
+            }
+        });
+        return $('#titular-form').valid();
+    },
+    salir: function () {
+        window.open(sprintf('TitularesGeneral.html'), '_self');
+    }
+}
+
+
---
+++ public/js/app/emisoresGeneral.js
@@ -0,0 +1,68 @@
+﻿/*
+ titulares.js
+ Funciones propias de la página Titulares.html
+*/
+
+var usuario = apiComunGeneral.obtenerUsuario();
+var data = null;
+
+var apiTitularesGeneral = {
+    ini: function () {
+        apiComunGeneral.initPage(usuario);
+        apiComunAjax.establecerClave(usuario.apiKey);
+        $('#titulares').attr('class', 'active');
+        $('#titulares-form').submit(function () { return false; });
+        apiTitularesGeneral.iniTitularesTabla();
+        apiTitularesGeneral.cargarTitulares();
+        $('#btnNuevo').click(apiTitularesGeneral.nuevo);
+    },
+    iniTitularesTabla: function () {
+        var options = apiComunGeneral.initTableOptions('dt_titulares', usuario.codigoIdioma);
+        options.data = data;
+        options.columns = [{
+            data: "titularId"
+        }, {
+            data: "nombreRazon"
+        }, {
+            data: "nifTitular"
+        }, {
+            data: "titularId",
+            render: function (data, type, row) {
+                var bt1 = "<button class='btn btn-circle btn-danger btn-lg' onclick='apiTitularesGeneral.eliminar(" + data + ");' title='Eliminar registro'> <i class='fa fa-trash-o fa-fw'></i> </button>";
+                var bt2 = "<button class='btn btn-circle btn-success btn-lg' onclick='apiTitularesGeneral.editar(" + data + ");' title='Editar registro'> <i class='fa fa-edit fa-fw'></i> </button>";
+                var html = "<div class='pull-right'>" + bt1 + " " + bt2 + "</div>";
+                return html;
+            }
+        }];
+        var tabla = $('#dt_titulares').DataTable(options);
+        tabla.columns(0).visible(false);
+    },
+    cargarTitulares: function () {
+        apiComunAjax.llamadaGeneral("GET", myconfig.apiUrl + "/api/titulares", null, function (err, data) {
+            if (err) return;
+            apiTitularesGeneral.cargarTitularesTabla(data);
+        });
+    },
+    cargarTitularesTabla: function (data) {
+        var dt = $('#dt_titulares').dataTable();
+        dt.fnClearTable();
+        if (data.length > 0) dt.fnAddData(data);
+        dt.fnDraw();
+    },
+    nuevo: function(){
+        window.open(sprintf('TitularesDetalle.html?id=%s', 0), '_self');
+    },
+    editar: function(id){
+        window.open(sprintf('TitularesDetalle.html?id=%s', id), '_self');
+    },
+    eliminar: function(id){
+        apiComunNotificaciones.mensajeAceptarCancelar(i18n.t("eliminar_pregunta"),function(){
+            apiComunAjax.llamadaGeneral("DELETE", myconfig.apiUrl + "/api/titulares/" + id, null, function(err){
+                if (err) return;
+                apiTitularesGeneral.cargarTitulares();
+            })
+        }, function(){})
+    }
+}
+
+
